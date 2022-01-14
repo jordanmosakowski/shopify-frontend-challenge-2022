478603afb4ed24842b8a68bfb339ab594aad99f4
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2022-01-13 19:02:46 -0800

    Added pagination

--- src/Rover.js
+++ src/Rover.js
@@ -7,26 +7,43 @@ import "./rover.css";
 export default function Rover(){
     const [pictures, setPictures] = useState(null);
     const { roverId } = useParams();
-    const [selectedDate, setDate] = useState("2022-01-01")
+    const [selectedDate, setDate] = useState("2022-01-01");
+    const [page, setPage] = useState(1);
 
-    useEffect( () => {
+    //Fetch photos on load
+    useEffect(() => {
         fetchPhotos(selectedDate);
     },[]);
 
+    //Fetch the first set of photos for a given date
     const fetchPhotos = async (date) =>{
         const photos = await apiCall(`/rovers/${roverId}/photos`, {earth_date: date, page: 1});
         setPictures(photos.photos);
     }
 
+    //Fetch the first set of photos for a given date
+    const paginatePhotos = async () =>{
+        const photos = await apiCall(`/rovers/${roverId}/photos`, {earth_date: selectedDate, page: page+1});
+        if(photos.photos.length==0){
+            setPage(null);
+            return;
+        }
+        pictures.push(...photos.photos);
+        setPage(page+1);
+    }
+
+    //Triggered when the date input field is changed
+    //Fetches new photos with the new date
     const dateChanged = (event) => {
         const newDate = event.target.value;
         setDate(newDate);
+        setPage(1);
         fetchPhotos(newDate);
     }
 
 
     return <div>
-        <div class='date-selection'>
+        <div className='date-selection'>
             <h2>View photos for:</h2>
             <input type="date" min="2004-01-01" max={(new Date()).toISOString().split("T")[0]} value={selectedDate}  onChange={dateChanged}/>    
         </div>
@@ -36,5 +53,8 @@ export default function Rover(){
                 (pictures.length==0 ? <h2>No photos found for selected date</h2> : pictures.map(picture => <Picture picture={picture} key={picture.id}/>))
             }
         </main>
+        {
+            page!=null && pictures!=null && pictures.length>0 && <button className='paginate' onClick={paginatePhotos}>Load More</button>
+        }
     </div>
 }
